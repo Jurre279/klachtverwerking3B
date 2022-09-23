aa1002520830aa9f018cbb3a1434e3945fb12fa1
Jurre279 <103561287+Jurre279@users.noreply.github.com>
2022-09-23 13:58:05 +0200

    Initial commit

---
+++ src/process.php
@@ -0,0 +1,41 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+include 'prive.php';
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = $bedrijfsemail;                     //SMTP username
+    $mail->Password   = $wachtwoord;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($bedrijfsemail, 'Klantenserives');
+    $mail->addAddress($_POST['Email'], $_POST['Naam']);     //Add a recipient            //Name is optional;
+    $mail->addCC($bedrijfsemail);
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Klachtverwerking';
+    $mail->Body    = $_POST['bericht'];
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
